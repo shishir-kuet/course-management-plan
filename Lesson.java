bd41407b08097e503f0d3b232ea8c73b459b759c
Md. Mosaddek Ali <mosaddekshishir4113@gmail.com>
2025-09-21 14:03:10 +0600

    Creating conflict

--- Lesson.java
+++ Lesson.java
@@ -1,16 +1,15 @@
 public class Lesson implements CourseComponents {
     private String title;
     private String duration;
-    private String instructorName;
+    
 
     public Lesson(String title, String duration, String instructorName ){
         this.title = title;
         this.duration = duration;
-        this.instructorName =  instructorName;
     }
 
     public void showDetails(){
-        System.out.println("Lesson: " + title + "| Duration: "+duration+ "| Instructor: " + instructorName);
+        System.out.println("Lesson: " + title + "| Duration: "+duration);
     }
 
 }
